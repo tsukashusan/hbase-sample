646aedbd79ae449bd44b80d11a1cb08fad8379e1
Shuichi Tsukamoto <shtsukam@microsoft.com>
2017-06-06 17:06:07 +0900

    HBase sample source

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,27 +1,41 @@
 package example
 
-import org.apache.hadoop.hbase.{TableName,HBaseConfiguration}
+import org.apache.hadoop.hbase.{TableName,HBaseConfiguration,HTableDescriptor,HColumnDescriptor}
 import org.apache.hadoop.hbase.client.ConnectionFactory
 
 object Hello extends Greeting with App {
-  println(greeting)
-  //hbaseTest
-  def hbaseTest(){
-      println("Job Start!")
-      val tableName = TableName.valueOf("stock-prices");
-      val conf = HBaseConfiguration.create
-      conf.clear
-      conf.set("hbase.zookeeper.property.clientPort", "2181")
-      conf.set("hbase.zookeeper.quorum", "zk2-hbase.2risndr15ibellqxn2wi3yjyha.lx.internal.cloudapp.net,zk3-hbase.2risndr15ibellqxn2wi3yjyha.lx.internal.cloudapp.net,zk4-hbase.2risndr15ibellqxn2wi3yjyha.lx.internal.cloudapp.net")
-      conf.set("zookeeper.znode.parent", "/hbase-unsecure")
-      conf.set("hbase.cluster.distributed", "true")
-      val conn = ConnectionFactory.createConnection(conf)
-      println(conf)
-      val admin = conn.getAdmin()
-      if (!admin.tableExists(tableName)) {
-         println(conf)
+  hbaseTest(args)
+  def hbaseTest(parameter:Array[String]){
+    val quorum = parameter.mkString(",")
+    println("quorum:%s".format(quorum))
+    println("Job Start!")
+    val tableName = TableName.valueOf("stock-prices");
+    val conf = HBaseConfiguration.create
+    conf.set("hbase.zookeeper.quorum", quorum)
+    conf.set("zookeeper.znode.parent", "/hbase-unsecure")
+    val conn = ConnectionFactory.createConnection(conf)
+    println(conf)
+    val admin = conn.getAdmin
+    val tableNames = admin.listTableNames
+    tableNames foreach { (t) =>{ 
+        if(t.compareTo(tableName) == 0){
+          admin.disableTable(t)
+          admin.deleteTable(t)
+          println("table %s deleted".format(t.getNameAsString))
+        }
       }
-       println("Job Finish!")
+    }
+    if (!admin.tableExists(tableName)) {
+       val table = new HTableDescriptor(tableName)
+       table.addFamily(new HColumnDescriptor("id"))
+       admin.createTable(table)
+       println("table %s created!".format(tableName.getNameAsString))
+       admin.disableTable(tableName)
+       println("table %s disabled!".format(tableName.getNameAsString))
+       admin.deleteTable(tableName)
+       println("table %s deleted!".format(tableName.getNameAsString))
+    }
+    println("Job Finish!")
   }
   def runMain(params:List[String]): Unit = {
       params foreach println
